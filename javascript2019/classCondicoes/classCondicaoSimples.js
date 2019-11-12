de50b313e8279dbed47124127f18d78b95c230cf
alexandre <alexandresccp@gmail.com>
2019-11-11 21:34:28 -0300

    condiçoes simples e compostas

---
+++ javascript2019/classCondicoes/classCondicaoSimples.js
@@ -0,0 +1,15 @@
+var vel = 50.5
+
+if (vel > 60) {
+    console.log(`Sua velocidade foi ${vel}... MULTADO`)
+} else {
+    console.log(`Sua velocidade foi ${vel}... SEGUE O BAILE`)
+}
+
+var country = 'BRASIL'
+
+if (country != 'BRASIL') {
+    console.log('TU É ESTRANGEIRO MEU CHAPA... WELCOME TO BRASIL.')
+} else {
+    console.log('BRASIL MEU BRASIL BRASILEIRO......')
+}
