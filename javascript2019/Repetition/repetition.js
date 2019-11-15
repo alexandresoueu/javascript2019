6ea4a6918fe6cb2793a791772ea355bd1dcce2cf
alexandre <alexandresccp@gmail.com>
2019-11-15 12:18:45 -0300

    Loop with while and dowhile

---
+++ javascript2019/Repetition/repetition.js
@@ -0,0 +1,13 @@
+var count = 6
+var bubba = 0
+
+do {
+    console.log(`Bubba is ${bubba}`)
+    bubba++
+} while(bubba <= 5)
+
+while(count <= 10) {
+    console.log(`Count is ==  ${count}`)
+    count++
+}
+
